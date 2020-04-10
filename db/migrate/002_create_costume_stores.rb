9633304a0d83f0b5f2ddff7425930474bbad86b2
Samantha Jordan <shinyguns@gmail.com>
2020-04-10 20:39:21 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,17 @@
 # Create your costume_stores migration here
+
+class CostumeStores < ActiveRecord::Base
+
+def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.interger :num_of_costumes
+      t.interger :num_of_employees
+      t.boolean :still_in_business
+      t.interger :opening_time
+      t.interger :closing_time
+    end
+  end
+
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class HauntedHouses < ActiveRecord::Base
+
+def change
+    create_table :costume_stores do |t|
+      t.integer :name 
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.boolean :family_friendly
+      t.integer :opening_date
+      t.integer :closing_date
+      t.string :long_desc
+
+end
